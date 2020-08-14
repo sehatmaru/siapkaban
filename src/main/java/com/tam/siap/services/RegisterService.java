1dc2f147f13a43edb1420c9584b493bad19af3c6
sehatmaru <maruzhaky@gmail.com>
2020-08-14 18:03:19 +0700

    added email config & service

--- src/main/java/com/tam/siap/services/RegisterService.java
+++ src/main/java/com/tam/siap/services/RegisterService.java
@@ -1,13 +1,28 @@
 package com.tam.siap.services;
 
-import com.tam.siap.document.DocumentPrinter;
-import com.tam.siap.models.*;
+import com.tam.siap.models.Account;
+import com.tam.siap.models.DPerusahaan;
+import com.tam.siap.models.DPribadi;
+import com.tam.siap.models.request.EmailRequestDto;
+import com.tam.siap.services.master.AccountService;
 import com.tam.siap.services.master.DataPerusahaanService;
 import com.tam.siap.services.master.DataPribadiService;
-import com.tam.siap.services.master.AccountService;
 import com.tam.siap.utils.EmailSMTP;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,6 +44,12 @@ public class RegisterService {
 
     DocumentPrinter documentPrinter = new DocumentPrinter();
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private Configuration configuration;
+
     public int register(Account account, DPribadi dPribadi, DPerusahaan dPerusahaan){
         int result = FAILED;
         int id = 1;
@@ -45,12 +66,40 @@ public class RegisterService {
                         EmailSMTP emailSMTP = new EmailSMTP();
                         emailSMTP.sendEmail(subject, content, to);
                         result = SUCCESS;
-                    };
+                    }
                 }
             }
         }
 
         return result;
+    }
+
+    public String sendMail(EmailRequestDto request, Map<String, String> model) {
+
+        String response;
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
+
+//            ClassPathResource pdf = new ClassPathResource("static/attachment.pdf");
+//            ClassPathResource image = new ClassPathResource("static/asbnotebook.png");
+            Template template = configuration.getTemplate("email.ftl");
+            String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+
+            helper.setTo(request.getTo());
+            helper.setFrom(request.getFrom());
+            helper.setSubject(request.getSubject());
+            helper.setText(html, true);
+//            helper.addInline("asbnotebook", image);
+//            helper.addAttachment("attachment.pdf", pdf);
+
+            mailSender.send(message);
+            response = "Email has been sent to :" + request.getTo();
+        } catch (MessagingException | IOException | TemplateException e) {
+            response = "Email send failure to :" + request.getTo() + ", messaging = " + e.toString();
+        }
+        return response;
     }
 
     public void printPdf(){
---
+++ src/main/resources/templates/email.ftl
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Spring boot send mail example</title>
+</head>
+<body>
+<h2>Sending mail from Spring boot!!</h2>
+<h3>Hi ${name}!!</h3>
+<p>${value}</p>
+<div>
+    Haiiii
+</div>
+</body>
+</html>
