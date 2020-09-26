7283078b5ce6766f290fab36639214869fb9f5d8
sehatmaru <maruzhaky@gmail.com>
2020-09-27 01:27:23 +0700

    file attached update

--- src/main/java/com/tam/siap/services/EmailService.java
+++ src/main/java/com/tam/siap/services/EmailService.java
@@ -99,7 +99,7 @@ public class EmailService {
 
             for (Dokumen doc : docs) {
                 File file = new File(doc.getPath());
-                helper.addAttachment(doc.getNamaDokumen() + ".docx", file);
+                helper.addAttachment(doc.getNamaDokumen() + ".pdf", file);
             }
 
             mailSender.send(message);
